35180f5f9a70fb10e5ee61e2bd16ab08bdef4c2b
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-08-20 09:21:39 +0200

    route checks

--- app/actions/getBlogs.ts
+++ app/actions/getBlogs.ts
@@ -1,8 +1,50 @@
 import prisma from "@/app/libs/prismadb";
 
-export default async function getBlogs() {
+interface IParams {
+  blogId?: string;
+  posterId?: string;
+  title?: string;
+  category?: string;
+  tags?: any;
+  slug?: string;
+  published?: boolean;
+}
+
+export default async function getBlogs(
+  params: IParams
+) {
   try {
+    const { blogId, posterId, title, category, tags, slug, published } = params;
+
     const query: any = {};
+
+    if (blogId) {
+      query.blogId = blogId;
+    };
+
+    if (posterId) {
+      query.posterId = posterId;
+    }
+
+    if (title) {
+      query.title = title;
+    }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (tags) {
+      query.tags = tags;
+    };
+
+    if (slug) {
+      query.slug = slug;
+    }
+
+    if (published) {
+      query.published = published;
+    }
 
     const blogs = await prisma.blog.findMany({
       where: query,
