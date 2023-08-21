433c427ae43e580c873fd76286f0fb62cb185b60
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-08-21 15:45:23 +0200

    change productsids

--- app/libs/subscriptionPlans.ts
+++ app/libs/subscriptionPlans.ts
@@ -4,7 +4,7 @@ const subscriptionPlans = new Map([
     {
       priceInCents: 999,
       name: "Abonnement d'un mois",
-      productId: "price_1Ndz5RFgJQlKBjkeNon42oIX",
+      productId: "price_1NhUlaFgJQlKBjkeY3HvEt0g",
     },
   ],
   [
@@ -12,7 +12,7 @@ const subscriptionPlans = new Map([
     {
       priceInCents: 2499,
       name: "Abonnement de trois mois",
-      productId: "price_1NdzC3FgJQlKBjken3i8X4l2",
+      productId: "price_1NhUlaFgJQlKBjkeVMV6RMWp",
     },
   ],
   [
@@ -20,7 +20,7 @@ const subscriptionPlans = new Map([
     {
       priceInCents: 3999,
       name: "Abonnement de six mois",
-      productId: "price_1NdzC3FgJQlKBjkeyMPBTlHq",
+      productId: "price_1NhUlaFgJQlKBjkeZoDffJCX",
     },
   ],
 ]);
