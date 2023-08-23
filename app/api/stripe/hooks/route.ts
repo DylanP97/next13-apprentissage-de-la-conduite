4430057b52ab936d97e3800ea66dbd5dce17414f
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-08-23 14:26:36 +0200

    fix error route does not match the required types

--- app/api/stripe/hooks/route.ts
+++ app/api/stripe/hooks/route.ts
@@ -1,7 +1,7 @@
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 import { buffer } from 'micro'
 
-const handler = async (req: any, res: any) => {
+export const handler = async (req: any, res: any) => {
   const signature = req.headers["stripe-signature"];
   const signingSecret = process.env.STRIPE_SIGNING_SECRET;
   const reqBuffer = await buffer(req)
@@ -20,5 +20,3 @@ const handler = async (req: any, res: any) => {
 
   res.send({ received: true });
 };
-
-export default handler;
