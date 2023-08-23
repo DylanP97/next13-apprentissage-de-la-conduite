5e0da94ddfea5bb497b12c13ce3a1a083166045e
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-08-23 14:33:54 +0200

    change handler folder

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 import subscriptionPlans from "@/app/libs/subscriptionPlans";
 import handleEvent from "./handleEvent";
+import { buffer } from 'micro'
+
 
 export async function POST(request: Request) {
   try {
@@ -40,3 +42,24 @@ export async function POST(request: Request) {
 // stripe.on('submit_payment.success', () => {
 //   handleEvent(session, session.metadata.plan_id)
 // })
+
+
+export const handler = async (req: any, res: any) => {
+  const signature = req.headers["stripe-signature"];
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+  const reqBuffer = await buffer(req)
+
+  let event 
+
+  try {
+    event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret);
+  } catch (error: any) {
+    return res.status(400).send(`Webhook error: ${error?.message}`)
+  }
+
+  console.log({ event })
+
+
+
+  res.send({ received: true });
+};
