ca79ec86f86317d291b9bc991411321e4c5442c9
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-08-24 14:25:24 +0200

    with body-parser

--- app/api/stripe/hooks/route.ts
+++ app/api/stripe/hooks/route.ts
@@ -1,17 +1,21 @@
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 import { buffer } from "micro";
 import { NextResponse } from "next/server";
-
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
+import bodyParser from 'body-parser';
 
 export async function POST(req: any, res: any) {
   const signature = req.headers["stripe-signature"];
   const signingSecret = process.env.STRIPE_SIGNING_SECRET;
   const reqBuffer = await buffer(req);
+
+  const rawBody: any = await new Promise((resolve) => {
+    bodyParser.raw({ type: 'application/json' })(req, res, () => {
+      resolve(req.body);
+    });
+  });
+
+  const body = rawBody.toString();
+  console.log(body);
 
   let event;
 
@@ -27,3 +31,22 @@ export async function POST(req: any, res: any) {
   console.log({ event });
   return NextResponse.json({ received: true, status: 200 });
 }
+
+
+
+// export async function POST(req: any, res: any) {
+//   // Use the raw mode of the body-parser module to get the raw request body
+//   const rawBody = await new Promise((resolve) => {
+//     bodyParser.raw({ type: 'application/json' })(req, res, () => {
+//       resolve(req.body);
+//     });
+//   });
+
+//   // Convert the raw buffer to a string using toString()
+//   const body = rawBody.toString();
+
+//   // Do something with the request body
+//   console.log(body);
+
+//   return NextResponse.json({ received: true, status: 200 });
+// }
