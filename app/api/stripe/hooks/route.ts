5eaaf56d8d2e7b4d0550fb41649c31cfb20b6ca7
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-08-24 13:43:18 +0200

    NextResponse & post & config

--- app/api/stripe/hooks/route.ts
+++ app/api/stripe/hooks/route.ts
@@ -1,19 +1,29 @@
 const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY);
 import { buffer } from "micro";
+import { NextResponse } from "next/server";
 
-export default async function handler(req: any, res: any) {
-    const signature = req.headers["stripe-signature"];
-    const signingSecret = process.env.STRIPE_SIGNING_SECRET;
-    const reqBuffer = await buffer(req);
-  
-    let event;
-  
-    try {
-      event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret);
-    } catch (error: any) {
-      return res.status(400).send(`Webhook error: ${error?.message}`);
-    }
-  
-    console.log({ event });
-    res.send({ received: true, status: 200 });
-  };
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export async function POST(req: any, res: any) {
+  const signature = req.headers["stripe-signature"];
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+  const reqBuffer = await buffer(req);
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(reqBuffer, signature, signingSecret);
+  } catch (error: any) {
+    return NextResponse.json({
+      status: 400,
+      message: `Webhook error: ${error?.message}`,
+    });
+  }
+
+  console.log({ event });
+  return NextResponse.json({ received: true, status: 200 });
+}
