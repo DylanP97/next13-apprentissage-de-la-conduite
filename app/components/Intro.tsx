c989084b756878c9c7db48116e4b96aae6c0cc74
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-09-05 17:24:04 +0200

    animation switchuup

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -12,8 +12,7 @@ function Intro() {
                 <p>Apprentissage de la Conduite et de la Sécurité Routière est une plateforme novatrice dédiée à accompagner les élèves dans leur parcours d&apos;apprentissage de la conduite automobile. Des leçons interactives permettent aux élèves de renforcer leur compréhension des règles de conduite et des bonnes pratiques tout en cultivant une sensibilisation accrue à la sécurité sur les routes.</p>
                 <br />
                 <div className='intro-buttons'>
-                    <Button className='btn-30color' href="/inscription" type="submit">Inscription</Button>
-                    <Button className='btn-10color' href="/connexion" type="submit">Connexion</Button>
+                    <Button className='btn-10color' href="/connexion" type="submit">Inscription / Connexion</Button>
                 </div>
             </Col>
             <Col className='intro-photo' md="6" xl="6">
--- app/connexion/ConnexionMain.tsx
+++ app/connexion/ConnexionMain.tsx
@@ -5,13 +5,12 @@ import SignUp from './InscriptionClient'
 import SignIn from './ConnexionClient'
 
 const ConnexionMain = () => {
-    const [isSignIn, setIsSignIn] = useState(false);
-
+    const [isSignIn, setIsSignIn] = useState(true);
 
     return (
-        <div className='intro sign'>
-            <SignUp isSignIn={isSignIn} state={setIsSignIn} />
+        <div className={`intro sign`}>
             <SignIn isSignIn={isSignIn} state={setIsSignIn} />
+            <SignUp isSignIn={isSignIn} state={setIsSignIn} />
         </div>
     )
 };
