8ea6b3aa78b7552b4b1076054ca61fc191359129
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-09-20 20:48:57 +0200

    remove form for a simple div

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server";
 import { transporter, contactMail } from "@/app/libs/transporter";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 
-const postmark = require("postmark");
-const postmarkApp = new postmark.ServerClient(process.env.POSTMARK_API);
+// const postmark = require("postmark");
+// const postmarkApp = new postmark.ServerClient(process.env.POSTMARK_API);
 
 export async function POST(request: Request, response: any) {
   try {
@@ -16,19 +16,23 @@ export async function POST(request: Request, response: any) {
     const body = await request.json();
     const { email, firstName, message } = body.data;
 
-    const messageInfo = `\n---\n ${firstName} vous a contacté`;
-
-    const postmarkResponse = await postmarkApp.sendEmail({
-      From: process.env.POSTMARK_EMAIL,
-      To: process.env.POSTMARK_EMAIL,
-      Subject: messageInfo,
-      TextBody: message,
-      MessageStream: "outbound",
+    await transporter.sendMail(contactMail(email, firstName, message), function (error: any) {
+      if (error) return NextResponse.error();
     });
 
-    if (!postmarkResponse.ErrorCode) {
-      return NextResponse.json({ message : "hii" });
-    }
+    // const messageInfo = `\n---\n ${firstName} vous a contacté`;
+
+    // const postmarkResponse = await postmarkApp.sendEmail({
+    //   From: process.env.POSTMARK_EMAIL,
+    //   To: process.env.POSTMARK_EMAIL,
+    //   Subject: messageInfo,
+    //   TextBody: message,
+    //   MessageStream: "outbound",
+    // });
+
+    // if (!postmarkResponse.ErrorCode) {
+    //   return NextResponse.json({ message : "hii" });
+    // }
 
     return NextResponse.json({ message : "success" });
   } catch (error: any) {
