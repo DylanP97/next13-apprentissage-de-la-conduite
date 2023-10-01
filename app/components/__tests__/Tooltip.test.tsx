ead5cf8a3b28a9abfb27ab7a186c7dc48686546e
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-10-01 14:37:17 +0200

    starting tests

---
+++ app/components/__tests__/Tooltip.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import Tooltip from '../Tooltip';
+
+import React from 'react'
+
+describe('Tooltip', () => {
+
+    it("renders Supprimer l'utilisateur without crashing", () => {
+
+        const message = "Supprimer l'utilisateur";
+        render(<Tooltip message={message}><h3>icon</h3>{message}</Tooltip>); // ARRANGE
+
+        const tooltipC = screen.queryByText("Supprimer l'utilisateur"); // ACT
+
+        expect(tooltipC).toBeInTheDocument(); // ASSERT
+    });
+
+    it('renders YOYOYOYYOYO without crashing', () => {
+
+        const message = "YOYOYOYYOYO";
+        render(<Tooltip message={message}><h3>icon</h3>{message}</Tooltip>); // ARRANGE
+
+        const tooltipC = screen.queryByText("YOYOYOYYOYO"); // ACT
+
+        expect(tooltipC).toBeInTheDocument(); // ASSERT
+    });
+})
