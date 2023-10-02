558e9c65c3546101e31b974ca8d03569def93a52
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-10-02 16:32:37 +0200

    implementing msw for testing api calls

--- app/components/__tests__/HomePage.test.tsx
+++ app/components/__tests__/HomePage.test.tsx
@@ -14,15 +14,83 @@ const mockCurrentUser = {
     subscriptionPlan: 5,
 }
 
-const mockBlogs = "";
+const mockBlogs = {
+    blogs: [
+        {
+            createdAt: "dsdsd",
+            updatedAt: "dsdsd",
+            id: "dsdsd3345345",
+            title: "Thisiesisdfd",
+            content: "dsdsd",
+            author: {
+                createdAt: "dsdsd",
+                updatedAt: "dsdsd",
+                emailVerified: false,
+                id: "dsdsd",
+                firstName: "dsdsd",
+                lastName: "dsdsd",
+                name: "dsdsd",
+                email: "dsdsd",
+                hashedPassword: "<PASSWORD>",
+                subscriptionPlan: 5,
+            },
+        },
+        {
+            createdAt: "dsdsd",
+            updatedAt: "dsdsd",
+            id: "dsds32132123d",
+            title: "dsdsd",
+            content: "dsdsd",
+            author: {
+                createdAt: "dsdsd",
+                updatedAt: "dsdsd",
+                emailVerified: false,
+                id: "aadsqdsq",
+                firstName: "dsdsd",
+                lastName: "dsdsd",
+                name: "dsdsd",
+                email: "dsdsd",
+                hashedPassword: "<PASSWORD>",
+                subscriptionPlan: 5,
+            },
+        },
+        {
+            createdAt: "dsdsd",
+            updatedAt: "dsdsd",
+            id: "d876873sdsd",
+            title: "sdcdscs",
+            content: "dsdsd",
+            author: {
+                createdAt: "dsdsd",
+                updatedAt: "dsdsd",
+                emailVerified: false,
+                id: "dsdsd",
+                firstName: "dsdsd",
+                lastName: "dsdsd",
+                name: "dsdsd",
+                email: "dsdsd",
+                hashedPassword: "<PASSWORD>",
+                subscriptionPlan: 5,
+            },
+        }
+    ],
+}
 
 describe('HomePage', () => {
 
-    it("renders without crashing", () => {
-        render(<HomePage currentUser={mockCurrentUser} blogs={mockBlogs} />); // ARRANGE
+    it("renders main container", () => {
+        render(<HomePage currentUser={mockCurrentUser} blogs={[]} />); // ARRANGE
 
         const homepage = screen.queryByRole('main'); // ACT
 
         expect(homepage).toBeInTheDocument(); // ASSERT
+    });
+
+    it("should render 'Il n'y a pas d'articles' when array is empty", () => {
+        render(<HomePage currentUser={mockCurrentUser} blogs={[]} />);
+
+        const h1 = screen.getByText("Il n'y a pas d'articles");
+
+        expect(h1).toBeInTheDocument();
     });
 })
