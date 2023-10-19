4bf475a4602410c08926efa3c44e57b174a9c7f3
DylanP97 <dylan.pinheiro.1997@gmail.com>
2023-10-19 09:23:52 +0200

    Adding 3d model of a car

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -3,6 +3,10 @@
 import { Button } from "react-bootstrap";
 import photo1 from "@/public/images/conduite.jpeg";
 import Image from "next/image";
+import { Model } from "./Pols_gtr";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 function Intro() {
   return (
@@ -24,7 +28,23 @@ function Intro() {
         </div>
       </div>
       <div className="intro-photo">
-        <Image src={photo1} alt="" />
+        {/* <Image src={photo1} alt="" /> */}
+        <Canvas
+          camera={{ position: [10, 10, 10], fov: 100 }}
+          style={{
+            backgroundColor: '#1d1d29',
+            width: '100%',
+            height: '100vh',
+          }}
+        >
+          <ambientLight intensity={1.25} />
+          <ambientLight intensity={0.1} />
+          <directionalLight intensity={0.4} />
+          <Suspense fallback={null}>
+            <Model />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
       </div>
     </div>
   );
